24a20c245aa65f1e3860b09d5745bc1e825c374d
Ed Gibbs <edward_gibbs@yahoo.com>
2014-11-24 06:04:59 -0800

    Refactoring main example passed method.

--- lib/yertle_formatter.rb
+++ lib/yertle_formatter.rb
@@ -6,10 +6,24 @@ class YertleFormatter < RSpec::Core::Formatters::BaseTextFormatter
   RSpec::Core::Formatters.register self, :example_passed
 
   def example_passed(notification)
-    if notification.example.metadata[:execution_result].run_time > THRESHOLD
-      output.print "\u{1f422} "
+    if slow_spec?(notification)
+      print_turtle
     else
-      output.print "."
+      print_dot
     end
+  end
+
+  private
+
+  def slow_spec?(notification)
+    notification.example.metadata[:execution_result].run_time > THRESHOLD
+  end
+
+  def print_turtle
+    output.print "\u{1f422} "
+  end
+
+  def print_dot
+    output.print "."
   end
 end
