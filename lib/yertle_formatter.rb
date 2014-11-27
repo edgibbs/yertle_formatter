c811c793d2244b89ae3d3d523ceca4a0e286d8e4
Ed Gibbs <edward_gibbs@yahoo.com>
2014-11-27 07:45:48 -0800

    Add initial summary notification of slow tests

--- lib/yertle_formatter.rb
+++ lib/yertle_formatter.rb
@@ -3,20 +3,27 @@ require "rspec/core/formatters/base_text_formatter"
 class YertleFormatter < RSpec::Core::Formatters::BaseTextFormatter
   THRESHOLD = 0.1
 
-  RSpec::Core::Formatters.register self, :example_passed
+  RSpec::Core::Formatters.register self, :example_passed, :dump_summary
 
   def example_passed(notification)
-    if slow_spec?(notification)
+    if slow_spec?(notification.example)
       print_turtle
     else
       print_dot
     end
   end
 
+  def dump_summary(summary_notification)
+    output.puts "\n------"
+    summary_notification.examples.each do |example|
+      output.puts example.metadata[:execution_result].run_time if slow_spec?(example)
+    end
+  end
+
   private
 
-  def slow_spec?(notification)
-    notification.example.metadata[:execution_result].run_time > THRESHOLD
+  def slow_spec?(example)
+    example.metadata[:execution_result].run_time > THRESHOLD
   end
 
   def print_turtle
--- spec/yertle_formatter_spec.rb
+++ spec/yertle_formatter_spec.rb
@@ -8,29 +8,58 @@ describe YertleFormatter do
   let(:metadata) { { execution_result: execution_result } }
   let(:formatter) { YertleFormatter.new(output) }
 
-  context "with slow tests" do
-    before do
-      allow(notification).to receive(:example) { example }
-      allow(example).to receive(:metadata) { metadata }
-      allow(execution_result).to receive(:run_time) { 0.2 }
+  describe "#example_passed" do
+    context "with a slow test" do
+      before do
+        allow(notification).to receive(:example) { example }
+        allow(example).to receive(:metadata) { metadata }
+        allow(execution_result).to receive(:run_time) { 0.2 }
+      end
+
+      it "displays a turtle emoji" do
+        expect(output).to receive(:print).with("\u{1f422} ")
+        formatter.example_passed(notification)
+      end
     end
 
-    it "displays a turtle emoji" do
-      expect(output).to receive(:print).with("\u{1f422} ")
-      formatter.example_passed(notification)
+    context "with a fast test" do
+      before do
+        allow(notification).to receive(:example) { example }
+        allow(example).to receive(:metadata) { metadata }
+        allow(execution_result).to receive(:run_time) { 0.01 }
+      end
+
+      it "displays the default dot" do
+        expect(output).to receive(:print).with(".")
+        formatter.example_passed(notification)
+      end
     end
   end
 
-  context "with a fast test" do
-    before do
-      allow(notification).to receive(:example) { example }
-      allow(example).to receive(:metadata) { metadata }
-      allow(execution_result).to receive(:run_time) { 0.01 }
-    end
+  describe "#dump_summary" do
+    let(:fast_example) { instance_double("RSpec::Core::Example") }
+    let(:fast_execution_result) { instance_double("RSpec::Core::Example::ExecutionResult") }
+    let(:fast_metadata) { { execution_result: fast_execution_result } }
 
-    it "displays the default dot" do
-      expect(output).to receive(:print).with(".")
-      formatter.example_passed(notification)
+    let(:slow_example) { instance_double("RSpec::Core::Example") }
+    let(:slow_execution_result) { instance_double("RSpec::Core::Example::ExecutionResult") }
+    let(:slow_metadata) { { execution_result: slow_execution_result } }
+    let(:summary_notification) { instance_double("RSpec::Core::Notifications::SummaryNotification") }
+
+    context "with slow tests" do
+      before do
+        allow(summary_notification).to receive(:examples) { [fast_example, slow_example] }
+        allow(fast_example).to receive(:metadata) { fast_metadata }
+        allow(fast_execution_result).to receive(:run_time) { 0.01 }
+        allow(slow_example).to receive(:metadata) { slow_metadata }
+        allow(slow_execution_result).to receive(:run_time) { 0.2 }
+      end
+
+      it "displays a list of the slow tests" do
+        expect(output).to receive(:puts).with("\n------")
+        expect(output).to receive(:puts).with(0.2)
+        formatter.dump_summary(summary_notification)
+      end
     end
   end
 end
